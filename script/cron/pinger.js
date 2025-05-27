16b036eb75d07f1068f5b056f5ba793007938c95
Abhinav <abhichhrp@gmail.com>
2025-05-27 15:51:31 +0530

    fix for testing

--- script/cron/pinger.js
+++ script/cron/pinger.js
@@ -3,10 +3,12 @@ const axios = require("axios");
 require('dotenv').config();
 
 const url = process.env.URL
+const be_url = process.env.BE_URL
 
 const task = async () => {
   try {
     const response = await axios.get(url);
+    await axios.get(be_url)
     console.log("Status: ", response.status)
     console.log(`Route ${url} hit at`, new Date().toLocaleString());
   } catch (err) {
--- script/index.js
+++ script/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 require("./cron/pinger")
 const app = express();
 
+app.get("/",(req, res)=> {
+    console.log("cron job working")
+    return res.status(200).json({message:"cron job hit"})
+})
 
 app.listen("3000", (req,res)=> {
     console.log("server running on 3000");
